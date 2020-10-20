8f7d389bc8ec651091d7a68ca7f0687941e8b59b
Srinivas Kaza <srinivas@kaza.io>
2020-10-19 21:43:56 -0400

    Verify that gradients of neural nets are the same

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,10 @@
 import numpy as np
 import jax
+from jax import jit, grad
 import jax.numpy as jnp
 import haiku as hk
-
 import torch
+from torch.autograd import grad as torch_grad
 
 from reference import FlexibleNeRFModelTorch, torch_to_jax
 from nerf import FlexibleNeRFModel
@@ -45,3 +46,26 @@ def test_torch_to_jax():
     torch_out = net_torch(torch.from_numpy(x))
 
     assert np.allclose(torch_out.detach().numpy(), np.array(jax_out), atol=1e-7)
+
+    # now let's verify that the gradients are correct
+    jax_fn = lambda x, p: net_jax.apply(p, x).flatten().sum()
+
+    jax_params_grad = jit(grad(jax_fn, argnums=(1,)))(jnp.array(x), jax_params)[0]
+
+    torch_loss = torch_out.flatten().sum()
+    torch_loss.backward()
+
+    torch_grads = torch_to_jax(
+        {k: v.grad for k, v in net_torch.named_parameters()}, "flexible_ne_rf_model"
+    )
+
+    def recursive_compare(d1, d2):
+        assert(d1.keys() == d2.keys())
+        for key in d1.keys():
+            if isinstance(d1[key], dict):
+                assert isinstance(d2[key], dict)
+                recursive_compare(d1[key], d2[key])
+            else:
+                assert np.allclose(d1[key], d2[key], rtol=1e-3, atol=1e-7)
+
+    recursive_compare(jax_params_grad, torch_grads)

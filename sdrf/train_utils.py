c15408269d14f5af795bc0b9b6aca9fc99e883c0
Srinivas Kaza <srinivas@kaza.io>
2020-11-01 03:27:22 -0500

    Computes loss

--- sdrf/train_utils.py
+++ sdrf/train_utils.py
@@ -5,7 +5,33 @@ import functools
 import numpy as np
 import jax
 import jax.numpy as jnp
-from jax import jit, vmap
+from jax import jit, vmap, grad
+
+from .rendering import (
+    LinearSampler,
+    StratifiedSampler,
+    GaussianSampler,
+    ExponentialSampler,
+    gaussian_pdf,
+    render,
+)
+from util import map_batched_rng
+
+
+def eikonal_loss(sdf, pts, sdf_params):
+    # TODO: Rewrite this, this is clunky
+    sdf_grad = lambda pt: grad(lambda pt, paras: sdf(pt, paras).sum(), argnums=(0,))(
+        pt, sdf_params
+    )
+    return jnp.mean(
+        vmap(lambda pt: (1.0 - jnp.linalg.norm(sdf_grad(pt)) ** 2.0))(pts), axis=0,
+    )
+
+
+def manifold_loss(sdf, pts, sdf_params):
+    return jnp.mean(
+        vmap(lambda pt: jnp.exp(1e-2 * jnp.abs(sdf(pt, sdf_params))))(pts), axis=0
+    )
 
 
 def run_one_iter_of_sdrf(
@@ -26,14 +52,11 @@ def run_one_iter_of_sdrf(
     else:
         raise Exception("Invalid sampler type")
 
-    sigma = (
-        options.render.phi.initial_sigma
-        * options.render.phi.decay_factor
-        ** (iteration / options.render.phi.num_decay_steps)
+    num_decay_steps = options.render.phi.lr_decay * 1000
+    sigma = options.render.phi.initial_sigma * options.render.phi.lr_decay_factor ** (
+        iteration / num_decay_steps
     )
-    phi = lambda dist: gaussian_pdf(
-        jnp.maximum(dist, jnp.zeros_like(dist)), 0.0, sigma
-    )
+    phi = lambda dist: gaussian_pdf(jnp.maximum(dist, jnp.zeros_like(dist)), 0.0, sigma)
 
     render_fn = lambda ro, rd, rng: render(
         sampler,
@@ -41,18 +64,19 @@ def run_one_iter_of_sdrf(
         model.appearance,
         ro,
         rd,
-        params.geometry,
+        params,
         rng,
         phi,
         options.render,
     )
 
-    (rgb, depth, rng) = map_batched_rng(
+    (rgb, depth), rng = map_batched_rng(
         jnp.stack((ro, rd), axis=-1),
         lambda chunk_rng: render_fn(
             chunk_rng[0][:, 0], chunk_rng[0][:, 1], chunk_rng[1]
         ),
-        options.train.chunksize,
+        options.render.chunksize,
+        True,
         rng,
     )
 
--- train_sdrf.py
+++ train_sdrf.py
@@ -21,18 +21,46 @@ from tqdm import tqdm, trange
 
 from nerf import loader, sampler
 from util import get_ray_bundle
-from rendering import SDRFParams, SDRF
+from sdrf import (
+    SDRFParams,
+    SDRF,
+    Siren,
+    run_one_iter_of_sdrf,
+    eikonal_loss,
+    manifold_loss,
+)
 
 Losses = namedtuple("Losses", ["rgb_loss", "eikonal_loss", "inter_loss"])
 register_pytree_node(Losses, lambda xs: (tuple(xs), None), lambda _, xs: Losses(*xs))
 
 
 def init_networks(config, rng):
-    geometry_fn = hk.transform(lambda x: Siren(3, 1, 4, 32, False, "relu")(x))
-    appearance_fn = hk.transform(lambda x: Siren(6, 3, 4, 32, False, "relu")(x))
+    geometry_fn = hk.transform(
+        lambda x: Siren(
+            3,
+            1,
+            config.geometry.num_layers,
+            config.geometry.hidden_size,
+            config.geometry.outermost_linear,
+            config.geometry.activation,
+        )(x)
+    )
+    appearance_fn = hk.transform(
+        lambda pt, rd: Siren(
+            6,
+            3,
+            config.geometry.num_layers,
+            config.geometry.hidden_size,
+            config.geometry.outermost_linear,
+            config.geometry.activation,
+        )(jnp.concatenate((pt, rd), axis=-1))
+    )
 
     geometry_params = geometry_fn.init(rng[0], jnp.ones([3,]))
-    appearance_params = appearance_fn.init(rng[0], jnp.ones([6,]))
+    appearance_params = appearance_fn.init(rng[0], jnp.ones([3,]), jnp.ones([3,]))
+
+    geometry_fn = hk.without_apply_rng(geometry_fn)
+    appearance_fn = hk.without_apply_rng(appearance_fn)
 
     return (
         SDRF(
@@ -41,3 +69,107 @@ def init_networks(config, rng):
         ),
         SDRFParams(geometry=geometry_params, appearance=appearance_params),
     )
+
+
+def train_sdrf(config):
+    rng = jax.random.PRNGKey(config.experiment.seed)
+    rng, *subrng = jax.random.split(rng, 3)
+
+    sdrf, sdrf_params = init_networks(config.sdrf.model, subrng)
+
+    basedir = config.dataset.basedir
+    print(f"Loading images/poses from {basedir}...")
+    images, poses, intrinsics = loader(
+        Path(".") / basedir, config.dataset.filter_chain, jax.devices()[0],
+    )
+    print("...done!")
+
+    # TODO: figure out optimizer
+    num_decay_steps = config.sdrf.model.optimizer.lr_decay * 1000
+    init_adam, update, get_params = adam(
+        lambda iteration: config.sdrf.model.model.optimizer.initial_lr
+        * (
+            config.sdrf.model.model.optimizer.lr_decay_factor
+            ** (iteration / num_decay_steps)
+        )
+    )
+    optimizer_state = init_adam(sdrf_params)
+
+    logdir = (
+        Path("logs") / "sdrf" / "lego" / datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    )
+    logdir.mkdir(exist_ok=True)
+    writer = SummaryWriter(logdir.absolute())
+    (logdir / "config.yml").open("w").write(config.to_yaml())
+
+    rng, subrng_img = jax.random.split(rng, 2)
+    train_image_seq = jax.random.randint(
+        subrng_img,
+        shape=(config.experiment.train_iters,),
+        minval=0,
+        maxval=images["train"].shape[0],
+        dtype=jnp.uint32,
+    )
+
+    def loss_fn(subrng, params, image_id, iteration):
+        ray_origins, ray_directions, target_s = sampler(
+            images["train"][image_id],
+            poses["train"][image_id],
+            intrinsics["train"],
+            subrng[0],
+            config.dataset.sampler,
+        )
+
+        # pick a bunch of random points to use for eikonal/manifold loss
+        # TODO: Maybe also sample on the isosurface?
+        eikonal_samples = (
+            jax.random.uniform(subrng[1], (config.sdrf.eikonal.num_samples, 3))
+            * config.sdrf.eikonal.scale
+        )
+
+        manifold_samples = (
+            jax.random.uniform(subrng[2], (config.sdrf.manifold.num_samples, 3))
+            * config.sdrf.manifold.scale
+        )
+
+        # render
+        rgb, depth = run_one_iter_of_sdrf(
+            sdrf,
+            params,
+            ray_origins,
+            ray_directions,
+            iteration,
+            config.sdrf,
+            subrng[3],
+        )
+
+        rgb_loss = jnp.mean(((target_s[..., :3] - rgb) ** 2.0).flatten())
+
+        e_loss, m_loss = (
+            eikonal_loss(sdrf.geometry, eikonal_samples, sdrf_params.geometry),
+            manifold_loss(sdrf.geometry, manifold_samples, sdrf_params.geometry),
+        )
+
+        return rgb_loss + e_loss + m_loss
+
+    rng, *subrng = jax.random.split(rng, 5)
+    print(jit(loss_fn)(subrng, sdrf_params, train_image_seq[0], 0))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    config_args = parser.parse_args()
+
+    with open(config_args.config, "r") as f:
+        config_dictionary = yaml.load(f, Loader=yaml.FullLoader)
+    config = Box(config_dictionary)
+
+    train_sdrf(config)
+
+
+if __name__ == "__main__":
+    import cv2
+    import time
+
+    main()
